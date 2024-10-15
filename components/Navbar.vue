6524a3f850d7aad58c16b25915822fd438d8a7fc
KaungMon <kaungmon.dev@example.com>
2024-10-15 22:44:45 +0630

    For UI

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,21 +1,61 @@
 <template>
   <div class="container">
-    <div class="navbar">
-      <nuxt-link to="/dashboard">Dashboard</nuxt-link>
-      <nuxt-link to="/categories">Categories</nuxt-link>
-      <nuxt-link to="/library">Library</nuxt-link>
-      <nuxt-link to="/notifications">Notifications</nuxt-link>
+    <div
+      class="flex flex-column justify-content-between navbar px-5 py-2 h-full"
+    >
+      <div>
+        <!-- SECTION - logo -->
+        <div class="flex align-items-center">
+          <div class="inline-block mr-2">
+            <img class="w-4rem" src="/public/icons/Logo.svg" alt="Logo" />
+          </div>
+          <h3 class="inline-block">
+            <span>Admin</span>
+            <span class="block">Dashboard</span>
+          </h3>
+        </div>
+        <!-- !SECTION -->
+        <hr class="w-12" />
+        <!-- SECTION - menu -->
+        <div class="my-5 py-5 flex flex-column">
+          <nuxt-link class="text-white no-underline my-3" to="/dashboard">
+            <i class="pi pi-home" style="color: white"></i> Dashboard
+          </nuxt-link>
+          <nuxt-link class="text-white no-underline my-3" to="/categories">
+            <i class="pi pi-list" style="color: white"></i> Categories
+          </nuxt-link>
+          <nuxt-link class="text-white no-underline my-3" to="/library">
+            <i class="pi pi-book" style="color: white"></i> Library
+          </nuxt-link>
+          <nuxt-link class="text-white no-underline my-3" to="/notifications">
+            <i class="pi pi-bell" style="color: white"></i> Notifications
+          </nuxt-link>
+        </div>
+        <!-- !SECTION -->
+      </div>
+      <!-- SECTION - logout and help -->
+      <div class="flex flex-column">
+        <nuxt-link class="text-white no-underline my-3" to="/library">
+          <i class="pi pi-info-circle" style="color: white"></i> Help and Info
+        </nuxt-link>
+        <nuxt-link class="text-white no-underline my-3" to="/notifications">
+          <i class="pi pi-sign-out" style="color: white"></i> Logout
+        </nuxt-link>
+      </div>
+      <!-- !SECTION -->
     </div>
   </div>
 </template>
 
-<script></script>
-
 <style lang="scss" scoped>
 .container {
-  width: 250px;
-  height: calc(100vh - 50px);
-  background: lightgray;
+  height: 100%;
+  backdrop-filter: blur(5.5px);
+  -webkit-backdrop-filter: blur(5.5px);
+  background: rgba(193, 193, 193, 0.386);
+  border-radius: 20px;
+  backdrop-filter: blur(5.5px);
+  -webkit-backdrop-filter: blur(5.5px);
   .navbar {
     display: flex;
     flex-direction: column;
--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="container">
-    <div class="navbar"></div>
-    <div class="page">
-      <Navbar />
-      <NuxtPage />
+  <div class="container h-screen">
+    <div class="page p-3 h-full grid nested-grid">
+      <div class="col-2">
+        <Navbar />
+      </div>
+      <div class="col-10">
+        <Topbar />
+        <div class="wapper mt-3">
+          <NuxtPage />
+        </div>
+      </div>
     </div>
     <div class="footer">
       <!-- <Footer /> -->
@@ -19,5 +25,14 @@
   .page {
     display: flex;
   }
+}
+.wapper {
+  height: calc(100% - 13%);
+  backdrop-filter: blur(5.5px);
+  -webkit-backdrop-filter: blur(5.5px);
+  background: rgba(193, 193, 193, 0.386);
+  border-radius: 20px;
+  backdrop-filter: blur(5.5px);
+  -webkit-backdrop-filter: blur(5.5px);
 }
 </style>
